504b38d8b12a588daae08691b34a0b3122cfb954
juig_ <juig_@DESKTOP-0OPHGMB>
2023-02-14 23:53:19 +0100

    Completado Opcion

---
+++ src/main/java/org/iesalandalus/programacion/alquilervehiculos/vista/Opcion.java
@@ -0,0 +1,44 @@
+package org.iesalandalus.programacion.alquilervehiculos.vista;
+
+public enum Opcion {
+	
+	SALIR("Salir"), 
+	INSERTAR_CLIENTE("Insertar cliente"), 
+	INSERTAR_TURISMO("Insrtar turismo"), 
+	INSERTAR_ALQUILER("Insrtar alquiler"),
+	BUSCAR_CLIENTE("Buscar cliente"),
+	BUSCAR_TURISMO("Buscar turismo"),
+	BUSCAR_ALQUILER("Buscar alquiler"),
+	MODIFICAR_CLIENTE("Modificar cliente"),
+	DEVOLVER_ALQUILER("Devolver alquiler"),
+	BORRAR_CLIENTE("Borrar cliente"),
+	BORRAR_TURISMO("Borrar turismo"),
+	BORRAR_ALQUILER("Borrar alquiler"),
+	LISTAR_CLIENTES("Listar clientes"),
+	LISTAR_TURISMOS("Listar turismos"),
+	LISTAR_ALQUILERES("Listar alquileres"),
+	LISTAR_ALQUILERES_CLIENTE("Listar alquileres de cliente"),
+	LISTAR_ALQUILERES_TURISMO("Listar alquileres de turismo");
+	
+	private String texto;
+	
+	private Opcion(String texto) {
+		this.texto = texto;
+	}
+	
+	public boolean esOrdinalValido(int ordinal) {
+		return ordinal >= 0 && ordinal < Opcion.values().length ? true : false;
+	}
+	
+	public Opcion get(int ordinal) {
+		if(!esOrdinalValido(ordinal)) {
+			throw new IllegalArgumentException("ERROR: Ordinal no valido");
+		}
+		return Opcion.values()[ordinal];
+	}
+	
+	public String toString() {
+		return String.format("%d.- %s",ordinal(),texto);
+	}
+
+}
